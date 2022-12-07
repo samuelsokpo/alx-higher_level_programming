ef71c186466f2409e739d6db507edec2804c09d5
samuelsokpo <samuel.sunny7@icloud.com>
2022-12-07 10:52:39 -0800

    final

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,4 +8,5 @@ def roman_to_int(roman_string):
     digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     for roman in reversed(roman_string):
         arabic = digits[roman]
-        total += arabic 
+        total += arabic if total < arabic * 5 else -arabic
+    return total
